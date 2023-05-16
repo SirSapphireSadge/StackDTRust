e4a0b7b21e5313fc4a5b79132a5d3527e6d5cb2c
Logan <133573052+SirSapphireSadge@users.noreply.github.com>
2023-05-17 01:17:01 +0200

    Create main.rs

---
+++ main.rs
@@ -0,0 +1,47 @@
+#[derive(Debug)]
+struct Stack<T> {
+    data: Vec<T>,
+}
+
+impl<T> Stack<T> {
+    fn new() -> Self {
+        Stack { data: Vec::new() }
+    }
+
+    fn is_empty(&self) -> bool {
+        self.data.is_empty()
+    }
+
+    fn push(&mut self, item: T) {
+        self.data.push(item);
+    }
+
+    fn pop(&mut self) -> Option<T> {
+        self.data.pop()
+    }
+
+    fn peek(&self) -> Option<&T> {
+        self.data.last()
+    }
+}
+
+// Example usage:
+fn main() {
+    let mut stack: Stack<i32> = Stack::new();
+
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+
+    println!("Is stack empty? {}", stack.is_empty()); // Output: false
+
+    println!("Stack peek: {:?}", stack.peek()); // Output: Some(3)
+
+    println!("Popped value: {:?}", stack.pop()); // Output: Some(3)
+
+    println!("Popped value: {:?}", stack.pop()); // Output: Some(2)
+
+    println!("Popped value: {:?}", stack.pop()); // Output: Some(1)
+
+    println!("Is stack empty? {}", stack.is_empty()); // Output: true
+}
